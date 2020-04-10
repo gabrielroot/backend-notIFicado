da9b8ec7cdd5b4a9fecf706d15be5eedbafe416c
gabrielroot <gabrielfer.s88@gmail.com>
2020-04-10 18:44:29 -0300

    conn

--- src/data/db.js
+++ src/data/db.js
@@ -2,5 +2,6 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: false,
 })
+//?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
