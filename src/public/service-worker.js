ee30fb98016b7ee4bc6b67905d740f3605c7fabb
gabrielroot <gabrielfer.s88@gmail.com>
2020-04-11 12:45:40 -0300

    função fetch no worker

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,9 +1,9 @@
 const cacheName = 'cache-v1';
 const precacheResources = [
   '/',
-  '/js/status.js',
   '/sobre',
   '/offline',
+  '/js/status.js',
   '/css/index.css',
   '/css/sobre.css',
 ];
@@ -22,28 +22,40 @@ self.addEventListener('activate', event => {
   console.log('Service worker activate event!');
 });
 
-addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('/offline');
-                });
-            });
+// addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;     // if valid response is found in cache return it
+//         } else {
+//           return fetch(event.request)     //fetch from internet
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());    //save the response for future
+//                   return res;   // return the fetched data
+//                 })
+//             })
+//             .catch(function(err) {       // fallback mechanism
+//               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+//                 .then(function(cache) {
+//                   return cache.match('/offline');
+//                 });
+//             });
+//         }
+//       })
+//   );
+// }); 
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
         }
+        return fetch(event.request);
       })
-  );
-});   
+    );
+});
