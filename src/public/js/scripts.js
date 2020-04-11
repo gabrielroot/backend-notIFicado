59a9f1d823321e85089abc57616f5dc47d2a32df
gabrielroot <gabrielfer.s88@gmail.com>
2020-04-11 10:34:38 -0300

    Teste notificações push

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -27,3 +27,15 @@ function init() {
       });
   }
 }
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        reg.showNotification('Hello world!');
+      });
+    }
+  }
