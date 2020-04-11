e6af19ff2a15aa26556ddeeb4764f205b1d0b05c
gabrielroot <gabrielfer.s88@gmail.com>
2020-04-11 15:33:45 -0300

    retrying push

--- src/data/db.js
+++ src/data/db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg')
 
-module.exports = new Pool({
-    user: 'grafite7_62',
-    password: '123',
-    host: 'localhost',
-    port: '5432',
-    database: 'notificado'
-})
-
 // module.exports = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: false,
+//     user: 'grafite7_62',
+//     password: '123',
+//     host: 'localhost',
+//     port: '5432',
+//     database: 'notificado'
 // })
+
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+})
 // //?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
