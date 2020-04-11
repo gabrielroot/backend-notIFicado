1a780475814989e738ba7a14801e788d9011161c
gabrielroot <gabrielfer.s88@gmail.com>
2020-04-10 21:01:31 -0300

    liberando a função para preencher o bd

--- src/controller/Scrap.js
+++ src/controller/Scrap.js
@@ -200,5 +200,6 @@ async function scrapBanner(){
 
 module.exports = setInterval(checkSaveNew, 60*60000)   //Executa a função de 60 em 60 minutos
 module.exports = setInterval(scrapBanner, 24*60*60000)   //Executa a função a cada 24h
-// checkSaveNew()
+checkSaveNew()
+scrapBanner()
 // module.exports = setInterval(checkNew, 30000)   //Executa a função de 30 em 30 segundos  [TESTE DE STRESS]
