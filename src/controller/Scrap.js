e4fd0401043cafc9b53551a35fe37ec2c8845a4c
gabrielroot <gabrielfer.s88@gmail.com>
2020-05-06 07:42:59 -0300

    scrap

--- src/controller/Scrap.js
+++ src/controller/Scrap.js
@@ -238,7 +238,7 @@ async function scrapBanner(){
 module.exports = setInterval(checkSaveNew, 60*60000)   //Executa a função de 60 em 60 minutos
 module.exports = setInterval(scrapBanner, 24*60*60000)   //Executa a função a cada 24h
 
-// checkSaveNew()
-// scrapBanner()
+checkSaveNew()
+scrapBanner()
 
 // module.exports = setInterval(checkNew, 30000)   //Executa esta função de 30 em 30 segundos  [TESTE DE STRESS]
